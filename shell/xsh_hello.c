cb6ce80586387b84e6ddfead7804eaadffd1c610
Rahul Sinha <rahsinha2@gmail.com>
2015-01-23 14:28:39 -0500

    1.2 quote changes

--- shell/xsh_hello.c
+++ shell/xsh_hello.c
@@ -18,13 +18,34 @@
  */
 shellcmd xsh_hello(int nargs, char *args[])
 {
+    int i = 0;
+    int j = 0;
+    /* Output help, if '--help' argument was supplied */
+    if (nargs == 2 && strcmp(args[1], "--help") == 0)
+    {
+        printf("Usage: %s\n\n", args[0]);
+        printf("Description:\n");
+        printf("\tDisplays input string\n");
+        printf("Options:\n");
+        printf("\t--help\tdisplay this help and exit\n");
+        return OK;
+    }
+
+    char * buffer = (char*) malloc (strlen (args[1]) * 2);
+    memset(buffer, 0, strlen (args[1]) * 2); 
+    for (i = j = 0; i<strlen (args[1]); i++) {
+   	if ((args[1][i] == '"' && i==0) || (args[1][i] == '"' && i == (strlen (args[1])-1))) 
+	    continue;
+	buffer[j++] = args[1][i];
+    }
 
     if (nargs == 2)
     {
-	printf("Hello %s, Welcome to the world of Xinu!!\n",args[1]);
+	printf("Hello %s, Welcome to the world of Xinu!!\n",buffer);
 	return OK;
     }
-
+	
+    free(buffer);
     /* Check for correct number of arguments */
     if (nargs > 2)
     {
